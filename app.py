c96ca09a6b5a380bd9bcdb2a7dca738bda02c3ad
wanjikum <wanjikumillicent5@gmail.com>
2017-02-14 20:34:52 +0300

    Adds user signup

--- app.py
+++ app.py
@@ -1,7 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, session, request, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from db_setup import Base, Issues, Users
+
+engine = create_engine('sqlite:///issue_tracker.db')
+
+Base.metadata.create_all(engine)
+DBSession =  sessionmaker(bind=engine)
+session = DBSession()
 
 @app.route('/')
 def index():
@@ -10,12 +20,28 @@ def index():
 
 @app.route('/signin')
 def sign_in():
-    """This function implements sign_in"""
+    """This function implements user/admin sign_in"""
+    
+
+
     return render_template("signin.html")
 
-@app.route('/signup')
+@app.route('/signup', methods=['GET', 'POST'] )
 def sign_up():
-    """This function implements sign_up"""
+    # """This function implements user/admin sign_up"""
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        email = request.form['email']
+        department = request.form['department']
+        designation = request.form['usertype']
+        newuser = Users(username=username, password=password, email=email, department=department, designation=designation)
+
+        session.add(newuser)
+        session.commit()
+        return redirect(url_for('index'))
+
+
     return render_template("signup.html")
 
 @app.route('/signin/raiseissue')
@@ -25,4 +51,4 @@ def raise_issue():
 
 
 if __name__ == '__main__':
-   app.run(host='0.0.0.0', port=8080)
+   app.run(host='0.0.0.0', port=8080, debug=True)
