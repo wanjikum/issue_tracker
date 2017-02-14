6a6c48761e453e209ee1d08e5ac3fc9061d78ecd
wanjikum <wanjikumillicent5@gmail.com>
2017-02-14 17:02:05 +0300

    Add the index page

--- app.py
+++ app.py
@@ -4,15 +4,19 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
 @app.route('/')
+def index():
+    """This function implements sign_in"""
+    return render_template("index.html")
+
+@app.route('/signin')
 def sign_in():
     """This function implements sign_in"""
     return render_template("signin.html")
 
 @app.route('/signup')
 def sign_up():
-    """This function implements sign_in"""
+    """This function implements sign_up"""
     return render_template("signup.html")
-
 
 if __name__ == '__main__':
    app.run(host='0.0.0.0', port=8080)
