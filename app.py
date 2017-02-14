222a24ecaec1b61a6011f0a041a7197857234ace
wanjikum <wanjikumillicent5@gmail.com>
2017-02-14 16:47:27 +0300

    Add a signup template

--- app.py
+++ app.py
@@ -8,5 +8,11 @@ def sign_in():
     """This function implements sign_in"""
     return render_template("signin.html")
 
+@app.route('/signup')
+def sign_up():
+    """This function implements sign_in"""
+    return render_template("signup.html")
+
+
 if __name__ == '__main__':
    app.run(host='0.0.0.0', port=8080)
