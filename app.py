1a50bf7b07c37616801a5e599db3995e1b55b723
wanjikum <wanjikumillicent5@gmail.com>
2017-02-14 16:39:15 +0300

    Add the sign in template

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
 
-#creating database models
-
-
+@app.route('/')
+def sign_in():
+    """This function implements sign_in"""
+    return render_template("signin.html")
 
 if __name__ == '__main__':
-   app.run()
+   app.run(host='0.0.0.0', port=8080)
